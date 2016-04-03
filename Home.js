f362847c89c8635cb03470a21c86135520fe702c
Alain Goldman <alain.goldman@gmail.com>
2016-04-02 23:07:51 -0400

    setup the flawless sidebar :)

--- Home.js
+++ Home.js
@@ -15,164 +15,107 @@ import Aboutios from './Aboutios';
 import ddpClient from './ddpClient';
 import Accounts from './accounts';
 import NavigationBar from 'react-native-navbar'; 
+import DrawerLayout from 'react-native-drawer-layout';
 
 
 class Home extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			email: 'alain.goldman@gmail.com',
-			loaded: true,
-			alert: "",
-		}
-	}
 
-	_handlePress(){
-		ddpClient.initialize()
-		  .then((res) => {
-		  	return Accounts.forgotPass(this.state.email.toLowerCase());
-		  })
-		  .then((res) => {
-		  	var newState = {};
-		  	newState["alert"] = "reset sent to email";
-		  	return this.setState(newState);
-		  })
-		  .catch((err) => {
-		    var newState = {};
-		    newState["alert"] = err.reason;
-		    return this.setState(newState);
-		  })
-	}
+  _handleChange(x, event) {
+    var newState = {};
+    newState[x] = event.nativeEvent.text;
+    this.setState(newState);
+  }
+  _openShelf(){
+    this.refs['DRAWER'].openDrawer();
+  }
 
-	_handleChange(x, event) {
-		var newState = {};
-		newState[x] = event.nativeEvent.text;
-		this.setState(newState);
-	}
-	_openShelf(){
-		//this.props.navigator.pop();
-		alert("Shelf opened");
-	}
-	render() {
-		const leftButtonConfig = 
-			<View>
-		      <Text style={styles.whiteArrow} onPress={this._openShelf.bind(this)} > {'|||'} </Text>
-		  	</View>;
+  render() {
+    const leftButtonConfig = 
+      <View>
+          <Text style={styles.whiteArrow} onPress={this._openShelf.bind(this)} > {'|||'} </Text>
+      </View>;
 
-		const titleConfig =
-			<View>
-		      <Text style={styles.navTitleText}>Browse</Text>
-		  	</View>;
+    const titleConfig =
+      <View>
+          <Text style={styles.navTitleText}>Browse</Text>
+      </View>;
 
+    const navigationView = (
+        <Image source={require('./dash.png')} style={styles.container}>
+          <View style={styles.siteWrap}>
+            <Image 
+              resizeMode="cover"
+              source={require('./logoalt.png')} 
+              style={styles.logo}/>
+            <Text style={styles.sidebarLink}> Browse </Text>
+            <Text style={styles.sidebarLink}> Messages </Text>
+            <Text style={styles.sidebarLink}> Listings </Text>
+            <Text style={styles.sidebarLink}> Orders </Text>
+            <Text style={styles.sidebarLink}> Purchases </Text>
+            <Text style={styles.sidebarLink}> Settings </Text>
+            <Text style={styles.sidebarLink}> Logout </Text>
+          </View>
+        </Image>);
 
-		return(
-			<View style={styles.contain}>
-				<NavigationBar
-				    title={titleConfig}
-				    tintColor="black"
-				    leftButton={leftButtonConfig} />
-			</View>
-		)
-	}
+    return(
+      <DrawerLayout 
+        drawerWidth={275}
+        ref={'DRAWER'}
+        drawerPosition={DrawerLayout.positions.Left}
+        renderNavigationView={() => navigationView}>
+        <View style={styles.contain}>
+          <NavigationBar
+            title={titleConfig}
+            tintColor="black"
+            leftButton={leftButtonConfig} />
+        </View>
+      </DrawerLayout>
+    )
+  }
 };
 
 
 
 var styles = StyleSheet.create({
-  // navbar: {
-  // 	alignSelf: "stretch",
-  // },
-  // midder: {
-  // 	alignItems: "center",
-  // 	marginTop: 20,
-  // },
-  // navy: {
-  // 	flex: 1,
-  // 	backgroundColor: "red",
-  // },
-  // wrap: {
-  // 	flex: 1,
-  // 	width: 280,
-  // },
-  // boxer: {
-  //   borderBottomWidth: 1,
-  //   borderColor: "white",
-  //   marginBottom: 16,
-  // },
-  // container: {
-  //   flex: 1,
-  //   width: null,
-  //   height: null,
-  //   alignItems: "center",
-  // },	
+  sidebarLink: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 17,
+    marginTop: 32,
+  },
+  siteWrap: {
+    flex: 1,
+    marginLeft: 55,
+    alignSelf: "flex-start",
+  },
+  logo: {
+    width: 30,
+    height: 30,
+    marginTop: 90,
+    marginBottom: 15,
 
-  // introText: {
-  // 	color: "white",
-  // 	fontSize: 11,
-  // 	alignSelf: "center",
-  // 	marginTop: 17,
-  // },
+  },
+  container: {
+    flex: 1,
+    width: null,
+    height: null,
+    alignItems: "center",
+  },  
   whiteArrow: {
-  	color: "white",
-  	fontSize: 22,
-  	marginTop: -5,
-  	marginLeft: 10,
+    color: "white",
+    fontSize: 22,
+    marginTop: -5,
+    marginLeft: 10,
   },
   navTitleText: {
-  	color: "white",
-  	fontSize: 18,
-  	marginBottom: 3.5,
+    color: "white",
+    fontSize: 18,
+    marginBottom: 3.5,
   },
   contain: {
-  	backgroundColor: "white",
-  	flex: 1,
+    backgroundColor: "white",
+    flex: 1,
   }
-  // larger: {
-  //   fontSize: 29,
-  // },
-  // backBlue: {
-  // 	flex: 1,
-  // 	backgroundColor: "white",
-  // },
-  // alert:{
-  // 	color: 'red',
-  // },
-  // input: {
-  // 	height: 35, 
-  // 	borderColor: 'gray', 
-  // 	borderBottomWidth: 1, 
-  // 	marginTop: 10,
-  // 	color: "white",
-  // },
-  // inputTitle: {
-  // 	color: "rgba(255,255,255,0.4)",
-  // 	fontSize: 12,
-  // 	height: 12,
-  // },
-  // butt: {
-  // 	backgroundColor: "rgb(14,185,125)",
-  // 	marginTop: 17,
-  // 	height: 40,
-  // 	justifyContent: 'center',
-  // 	alignItems: "center",
-  // },
-  // buttInner: {
-  // 	color: "white",
-  // 	fontSize: 11,
-  // 	justifyContent: 'center',
-  // },
-  // link: {
-  // 	color: "white",
-  // 	marginTop: 10,
-  // },
-  // bot: {
-  // 	width: 280,
-  // 	position: "absolute",
-  // 	alignSelf: "center",
-  // 	alignItems: "center",
-  // 	bottom: 0,
-  // 	paddingBottom: 20,
-  // }
 });
 
 module.exports = Home;
